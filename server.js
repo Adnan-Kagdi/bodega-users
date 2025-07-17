b873604c82f844ddac9dfe745e1be4c4421880d5
Adnan Kagdi <kagdiadnan123@gmail.com>
2025-07-17 16:12:48 +0530

    adding server file

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/user', userRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(5000, () => console.log('Server running on port 5000'));
+  })
+  .catch(err => console.log(err));
